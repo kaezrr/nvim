3ff354978198dc75b1bd6c74e70820fc0f40af3d
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-06 22:42:31 +0530

    add the best keybind of all time

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -26,3 +26,4 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', '<leader>r', '<cmd>restart<CR>', { desc = '[R]estart neovim' })
+vim.keymap.set('n', ';', ':', { desc = 'Big brain stuff' })
